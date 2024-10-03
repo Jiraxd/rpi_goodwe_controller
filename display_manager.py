40f0687d87143a86f2be0c77a81ce4aa395f9043
Jiraxd <123981279+Jiraxd@users.noreply.github.com>
2024-10-03 17:45:53 +0200

    Add offline mode, fix lcd manager, add data collection from inverter

--- display_manager.py
+++ display_manager.py
@@ -1,16 +1,21 @@
 from RPLCD.i2c import CharLCD
+from typing import List
+import asyncio
 
-class lcdManager:
+class LCDManager:
 
-    def __init__(self, _cols, _rows, _ip):
+    def __init__(self, _cols, _rows):
         self.lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1, cols=_cols, rows=_rows, dotsize=8)
+
+    async def write_init_message(self, _ip):
+        await asyncio.sleep(1)
         self.lcd.clear()
         self.lcd.write_string("Connecting...")
         self.lcd.lf()
         self.lcd.write_string("Inverter IP:")
         self.lcd.write_string(_ip)
     
-    def write_lines(self, text: list[str]):
+    def write_lines(self, text: List[str]):
         self.lcd.clear()
         for line in text:
             self.lcd.write_string(line)
--- main.py
+++ main.py
@@ -1,37 +1,50 @@
 import asyncio
 import goodwe
 import utils
-import display_manager
+from display_manager import LCDManager
 
 
 ip_address = '192.168.0.114'
 
 inverter: goodwe.Inverter = None
 
+lcdmanager : LCDManager = None
+
+offlineMode = False # Set to True for testing purposes without connection to goodwe inverter
+
 
 async def main():
-    lcdmanager = display_manager.lcdManager(20, 4, ip_address)
+    global lcdmanager
+    lcdmanager = LCDManager(20, 4)
+    await lcdmanager.write_init_message(ip_address)
 
-    failed = await try_connection(lcdmanager)
-    if(failed):
-        lcdmanager.write_lines(["Failed to connect!","Script disabled!"])
-        return
-    lcdmanager.write_lines(["Connected!", "Production: " + str(inverter.read_setting("grid_export")), "Export: " + str(inverter.read_setting("grid_export")),])
+    if offlineMode:
+        lcdmanager.write_lines(["Offline Mode", "Production: 0 kw", "Export: 0 kw"])
+    else:
+        failed = await try_connection()
+        if(failed):
+            lcdmanager.write_lines(["Failed to connect!","Script disabled!"])
+            return
+        data = await inverter.read_runtime_data()
+        print(data)
+        lcdmanager.write_lines(["Connected!", "Production: " + data.get("ppv", 0), "Export: " + data.get("active_power", 0), "House: " + data.get("house_consumption:", 0) + "W"])
 
 
 
-async def try_connection(lcdmanager, retries=0):
+async def try_connection():
     global inverter
-    if(retries == 3):
-        return True
-    try:
-        inverter = await goodwe.connect(ip_address)
-        await print_runtime_data()
-        return False
-    except Exception as e:
-        lcdmanager.write_lines(["Failed to connect!", "Retrying in 5s"])
-        await asyncio.sleep(5)
-        await try_connection(lcdmanager, retries + 1)
+    for i in range(3):
+        if(i == 3):
+            return True
+        try:
+            inverter = await goodwe.connect(ip_address)
+        #   await print_runtime_data()
+            return False
+        except Exception:
+            lcdmanager.write_lines(["Failed to connect!", "Retrying in 5s"])
+            await asyncio.sleep(5)
+
+    return False
 
 async def print_runtime_data():
     
@@ -42,4 +55,4 @@ async def print_runtime_data():
             print(f"{sensor.id_}: \t\t {sensor.name} = {runtime_data[sensor.id_]} {sensor.unit}")
 
 
-utils.run_async(main)
+asyncio.run(main())
