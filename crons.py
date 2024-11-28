19fed5014768310159e03b0cdfbcd765d0b7560d
Jiraxd <123981279+Jiraxd@users.noreply.github.com>
2024-11-28 19:14:35 +0100

    fixed crons erroring out

--- crons.py
+++ crons.py
@@ -1,17 +1,19 @@
 import pycron
 from decorators import error_handler
-from logger import LoggerCustom
+from functools import partial
 
 class CronManager:
-    def __init__(self, _logManager: LoggerCustom, _controller):
+    def __init__(self, _logManager, _controller):
         self.logManager = _logManager
         self.controller = _controller
         self.logManager.log("CronManager loaded!")
         
     def start(self):
+        pycron.add_job("* * * * * */15", partial(self.getDataAndWriteToLCD))
+        pycron.add_job("* * * * * */30", partial(self.checkWaterHeating))
+        pycron.add_job("* * * * * */60", partial(self.checkPrice))
         pycron.start()
 
-    @pycron.cron("* * * * * */15")
     @error_handler  
     async def getDataAndWriteToLCD(self):  
         self.logManager.log("Running cron getDataAndWriteToLCD()")
@@ -19,7 +21,6 @@ class CronManager:
         await self.controller.check_grid_limit(data)
         self.logManager.log("Cron getDataAndWriteToLCD() finished running!")
 
-    @pycron.cron("* * * * * */30")
     @error_handler  
     async def checkWaterHeating(self):  
         self.logManager.log("Running cron checkWaterHeating()")
@@ -27,7 +28,6 @@ class CronManager:
         await self.controller.check_water_heating(data)
         self.logManager.log("Cron checkWaterHeating() finished running!")
         
-    @pycron.cron("* * * * * */60")
     @error_handler
     async def checkPrice(self):
         self.logManager.log("Running cron checkPrice()")
