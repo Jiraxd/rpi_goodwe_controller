94915e6489fcf7d3b510de26370e2a44fd7a65b6
Jiraxd <123981279+Jiraxd@users.noreply.github.com>
2024-11-28 19:23:55 +0100

    NO WAY

--- crons.py
+++ crons.py
@@ -1,7 +1,7 @@
 import pycron
 import asyncio
 from decorators import error_handler
-from functools import partial
+from datetime import datetime
 
 class CronManager:
     def __init__(self, _logManager, _controller):
@@ -11,38 +11,27 @@ class CronManager:
         self.logManager.log("CronManager loaded!")
         
     def start(self):
-        
-        # Create wrapper functions that schedule coroutines in the existing event loop, pycron is passing DATETIME so we need to wrap it
-        def lcd_wrapper(dt):
-            return self.loop.create_task(self.getDataAndWriteToLCD())
-            
-        def heating_wrapper(dt):
-            return self.loop.create_task(self.checkWaterHeating())
-            
-        def price_wrapper(dt):
-            return self.loop.create_task(self.checkPrice())
-
-        pycron.add_job("* * * * * */15", lcd_wrapper)
-        pycron.add_job("* * * * * */30", heating_wrapper)
-        pycron.add_job("* * * * * */60", price_wrapper)
         pycron.start()
 
+    @pycron.cron("*/15 * * * * *")
     @error_handler  
-    async def getDataAndWriteToLCD(self):  
+    async def getDataAndWriteToLCD(self, timestamp: datetime):  
         self.logManager.log("Running cron getDataAndWriteToLCD()")
         data = await self.controller.get_data_and_write_to_lcd()
         await self.controller.check_grid_limit(data)
         self.logManager.log("Cron getDataAndWriteToLCD() finished running!")
 
+    @pycron.cron("*/30 * * * * *")
     @error_handler  
-    async def checkWaterHeating(self):  
+    async def checkWaterHeating(self, timestamp: datetime):  
         self.logManager.log("Running cron checkWaterHeating()")
         data = await self.controller.get_data()
         await self.controller.check_water_heating(data)
         self.logManager.log("Cron checkWaterHeating() finished running!")
         
+    @pycron.cron("*/60 * * * * *")
     @error_handler
-    async def checkPrice(self):
+    async def checkPrice(self, timestamp: datetime):
         self.logManager.log("Running cron checkPrice()")
         await self.controller.check_price_and_disable_enable_sell()
         self.logManager.log("Cron checkPrice() finished running!")
