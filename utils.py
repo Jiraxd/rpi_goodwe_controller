ef73662b3f19a2cf483de19cce7bc4d22f041a62
Jiraxd <123981279+Jiraxd@users.noreply.github.com>
2025-05-07 18:49:08 +0200

    fix hopefully

--- utils.py
+++ utils.py
@@ -7,28 +7,24 @@ async def disable_grid_export(inverter: goodwe.Inverter, logManager : LoggerCust
     return
     limit = await inverter.read_setting("grid_export_limit")
     if(limit != 0):
-        inverter.write_setting("grid_export_limit", 0)
+        await inverter.write_setting("grid_export_limit", 0)
     enabled = await inverter.read_setting("grid_export")
     if(enabled != 1):
-        inverter.write_setting("grid_export", 1)
+        await inverter.write_setting("grid_export", 1)
 
 async def enable_grid_export(inverter: goodwe.Inverter, logManager : LoggerCustom):
     logManager.log("starting export")
     return
     limit = await inverter.read_setting("grid_export_limit")
     if(limit != config.max_export_set):
-        inverter.write_setting("grid_export_limit", config.max_export_set)
+        await inverter.write_setting("grid_export_limit", config.max_export_set)
     
 async def disable_grid_limit(inverter: goodwe.Inverter, logManager : LoggerCustom):
     logManager.log("disabling limit")
    # return
     enabled = await inverter.read_setting("grid_export")
     logManager.log(f"enabled: {enabled}, type: {type(enabled)}")
-    inverter.write_setting("grid_export", 0)
-    enabled = await inverter.read_setting("grid_export")
-    logManager.log(f"enabled: {enabled}, type: {type(enabled)}")
-    settings = await inverter.read_settings_data()
-    logManager.log(f"settings: {settings}")
+    await inverter.write_setting("grid_export", 0)
 
 async def enable_grid_limit(inverter: goodwe.Inverter, logManager : LoggerCustom):
     logManager.log("enabling limit")
@@ -36,8 +32,8 @@ async def enable_grid_limit(inverter: goodwe.Inverter, logManager : LoggerCustom
     limit = await inverter.read_setting("grid_export_limit")
     logManager.log(f"limit: {limit}")
     if(limit != config.max_export_set):
-        inverter.write_setting("grid_export_limit", config.max_export_set)
+        await inverter.write_setting("grid_export_limit", config.max_export_set)
     enabled = await inverter.read_setting("grid_export")
     logManager.log(f"enabled: {enabled}, type: {type(enabled)}")
-    inverter.write_setting("grid_export", 1)
+    await inverter.write_setting("grid_export", 1)
 
